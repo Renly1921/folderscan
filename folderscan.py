4e6b456111bbca26cc590ccb19ccd20fb0af92c8
Renly <Renly@home.com>
2019-02-16 19:55:40 +0800

    optimized code with add function of mail_notification step1

--- folderscan.py
+++ folderscan.py
@@ -22,8 +22,12 @@ def rewrite_record(filename,record_filename_hr,record_filename_con):
     pickle.dump(record_filename_dict, f1)
     f1.close()
 
+# 发送电子邮件通知
+def mail_notification(mail_address, foldername):
+    return True
+
 # 主程序
-def run(target_dir_hr, target_dir_con, record_file, interval):
+def run(target_dir_hr, target_dir_con, record_file, interval, mail_address):
     # 程序运行前先读入之前一次运行的结果，存入字典中
     f1 = open(record_file, 'rb')
     try:
@@ -32,9 +36,12 @@ def run(target_dir_hr, target_dir_con, record_file, interval):
         # 当存储字典的文件为空文件或者格式不正确时，初始化字典数据
         record_filename_dict = {'hr':'nofile', 'consultant':'nofile'}
     f1.close()
+    # 下面2行代码为不从文件读取，假设每次启动程序时都已经手动检查过没有新文件
+    #record_filename_dict['hr'] = new_file_name_hr
+    #record_filename_dict['consultant'] = new_file_name_con:
     while True:
         try:
-            # sleep for the remaining seconds of interval
+            # 等待
             time_remaining = interval-time.time()%interval
             time.sleep(time_remaining)
             # 获取最新的生成的文件名
@@ -42,18 +49,18 @@ def run(target_dir_hr, target_dir_con, record_file, interval):
             new_file_name_con = check_latest_file(target_dir_con)
 
             if record_filename_dict['hr'] != new_file_name_hr:
-                rewrite_record(record_file,new_file_name_hr, new_file_name_con)
+                rewrite_record(record_file, new_file_name_hr, new_file_name_con)
                 print('find new file in HR folder')
-                #win32api.MessageBox(0,"this is messagebox", "title",MB_OK)
                 # 调用发消息函数通知对方
-                tkinter.messagebox.showinfo("CV Updated", "There is new CV file to check in HR folder")
+                #tkinter.messagebox.showinfo("CV Updated", "There is new CV file to check in HR folder")
+                mail_notification(mail_address, 'hr')
                 break
             elif record_filename_dict['consultant'] != new_file_name_con:
                 rewrite_record(record_file, new_file_name_hr, new_file_name_con)
                 print('find new file in Consultant folder')
-                # win32api.MessageBox(0,"this is messagebox", "title",MB_OK)
                 # 调用发消息函数通知对方
-                tkinter.messagebox.showinfo("CV Updated", "There is new CV file to check in Consultant foler")
+                #tkinter.messagebox.showinfo("CV Updated", "There is new CV file to check in Consultant foler")
+                mail_notification(mail_address, 'consultant')
                 break
             else:
                 print('no new file')
@@ -61,9 +68,10 @@ def run(target_dir_hr, target_dir_con, record_file, interval):
             print('error')
 
 
-target_dir_hr='D:\\Renly\\testfolder1'
-target_dir_con='D:\\Renly\\testfolder2'
-record_file='latest_file_name.txt'
-interval=5
+target_dir_hr = 'D:\\Renly\\testfolder1'
+target_dir_con = 'D:\\Renly\\testfolder2'
+record_file = 'latest_file_name.txt'
+interval = 300
+mail_address = 'xxx@xxx.com'
 
-run(target_dir_hr, target_dir_con, record_file,interval)
+run(target_dir_hr, target_dir_con, record_file, interval, mail_address)
